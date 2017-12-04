0354efd8977c7f56ef1b8ac6f3705e14b900934f
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2017-12-04 14:23:40 -0700

    round 2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      arr: [
+        'spaghetti',
+        'ice cream',
+        'sushi',
+        'bologna',
+        'cheese'
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{this.state.arr[0]}</h2>
+        <h2>{this.state.arr[1]}</h2>
+        <h2>{this.state.arr[2]}</h2>
+        <h2>{this.state.arr[3]}</h2>
+        <h2>{this.state.arr[4]}</h2>
       </div>
     );
   }
