67cd6a4b05deb8b25c0c7309b548a62c7ad5e974
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2017-12-06 08:46:13 -0700

    first commit by myself

--- app-5/src/Image.js
+++ app-5/src/Image.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 export default class Image extends Component{
     render(){
         return(
-            <img src="https://http.cat/101" alt='catpic'/>
+            <img src='https://http.cat/405' alt='catpic'/>
         )
     }
 }
